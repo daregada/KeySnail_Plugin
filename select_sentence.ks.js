a723f6f2bff6e00a723528337944afea6ca3e094
Koichi OIKE <daichi14657@gmail.com>
2011-03-20 08:40:40 +0900

    select_sentence 1.1 (support multiple ranges)

--- select_sentence.ks.js
+++ select_sentence.ks.js
@@ -1,10 +1,10 @@
 var PLUGIN_INFO =
 <KeySnailPlugin>
-    <name>Selection Sentence</name>
+    <name>Select Sentence</name>
     <name lang="ja">文を選択</name>
-    <description>Select a sentence around your selection (or caret)</description>
+    <description>Select a whole sentence around your selection (or caret)</description>
     <description lang="ja">セレクション(あるいはキャレット)周囲の文を選択します</description>
-    <version>1.0</version>
+    <version>1.1</version>
     <updateURL>http://github.com/daregada/KeySnail_Plugin/raw/master/select_sentence.ks.js</updateURL>
     <author mail="daichi14657@gmail.com" homepage="http://daregada.blogspot.com/">Daregada</author>
     <license>The MIT License</license>
@@ -16,21 +16,17 @@ var PLUGIN_INFO =
     <detail><![CDATA[
 === Usage ===
 ==== Setting ====
-
 You can paste code below to your .keysnail.js file.
-
 >|javascript|
-key.setViewKey('C-1',
+key.setGlobalKey('C-1',
     function (ev, arg) {
         ext.exec("select_sentence", arg);
-    }, ext.description("select_sentence"), true);
+    }, ext.description("Select Sentence"), true);
 ||<
-
-In this example, you can start select_sentence by pressing C-1 key in the view mode.
-
+In this example, you can start select_sentence by pressing C-1 key in any mode.
 ==== Action ====
-Click or drag any text-node and call select_sentence, and you can select automatically the sentence around caret (or selection).  You do NOT have to enter caret-mode (F7 key).
-This select_sentence searches head and tail of the sentence from the text-node that you click and its sibling nodes.
+Click (or drag) any text-node and call select_sentence, and you can select automatically the whole sentence around caret (or selection).  You do NOT have to enter caret-mode (F7 key).
+This select_sentence searches head and tail of the sentence from the clicked text-node and its sibling nodes.
 ==== Target languages ====
 - English et al.
 - Japanese
@@ -38,18 +34,20 @@ This select_sentence searches head and tail of the sentence from the text-node t
     <detail lang="ja"><![CDATA[
 === 使い方 ===
 ==== 起動 ====
-M-x (または ext.select() を呼出すキーバインド) から select_sentence を選ぶと起動します。
-次のようにして任意のキーへコマンドを割り当てておくことも可能です。
+次のようにして適当なキーに select_sentence を割り当ててください。
 >|javascript|
-key.setViewKey('C-1',
+key.setGlobalKey('C-1',
     function (ev, arg) {
         ext.exec("select_sentence", arg);
-    }, ext.description("select_sentence"), true);
+    }, ext.description("文を選択"), true);
 ||<
-例えば上記のような設定を初期化ファイル(.keysnail.jsなど)へ記述しておくことにより、ブラウズ画面で C-1 と押すことで起動します。
+たとえば、上記のような設定を初期化ファイル(.keysnail.jsなど)に記述しておけば C-1 で起動します。
 ==== アクション ====
-対象となるテキストの適当な位置をクリックしてから呼び出すと、その文の先頭と末尾を検索して文全体を選択します。いちいちF7キーを押してキャレットモードにする必要はありません。
+対象となるテキストの適当な位置をクリックしてから呼び出すと、その文の先頭と末尾を検索して文全体を選択します。F7キーでキャレットモードに切り替える必要はありません。
+
 検索対象は、クリックしたテキストノードおよびその兄弟ノードです。繰り返し呼び出すと、検索対象に含まれる次の文も選択に追加されます。
+
+なお、論理的な意味での文が、ヘッダーやフッターなどによって複数のパーツに分割されている場合は、ドラッグ(2箇所目以降はCtrl-ドラッグ)で、各パーツの一部を選択してから呼び出す必要があります。
 ==== 選択できる言語の種類と文末判定 ====
 - 英語 (および「.」と空白類で文末を示す他の言語)
 - 日本語 (および「。」または「．」で文末を示す他の言語)
@@ -58,10 +56,7 @@ key.setViewKey('C-1',
 
 function select_sentence() {
     var sentenceEndPattern = new RegExp(L('\\.\\s+|\\n{2,}|[。．]'), 'g');
-    var tailNode = null;
-    var tailIndex = -1;
-    var headNode = null;
-    var headIndex = -1;
+    var headNode, headIndex, tailNode, tailIndex;
 
     function searchSentenceHead(node, opt_offset) {
 	if (node.nodeType !== Node.TEXT_NODE) {
@@ -124,62 +119,74 @@ function select_sentence() {
 	return;
     }
 
-    var range = content.getSelection().getRangeAt(0);
-    if (range.startContainer.nodeType !== Node.TEXT_NODE ||
-	range.endContainer.nodeType !== Node.TEXT_NODE) {
-	alert('Selection (or caret) is not in any text node');
+    function stretchRange(range, index) {
+	if (range.startContainer.nodeType !== Node.TEXT_NODE ||
+	    range.endContainer.nodeType !== Node.TEXT_NODE) {
+	    alert('Selection (or caret) [' + range + '] is not in any text node');
+	    return;
+	}
+
+	headNode = tailNode = null;
+	headIndex = tailIndex = -1;
+
+	var currentNode = range.startContainer;
+	searchSentenceHead(currentNode, range.startOffset);
+	while (headNode === null && currentNode.previousSibling !== null) {
+	    currentNode = currentNode.previousSibling;
+	    searchSentenceHead(currentNode);
+	}
+	if (headNode === null) {
+	    headNode = currentNode;
+	    headIndex = 0;
+	}
+
+	currentNode = range.endContainer;
+	searchSentenceTail(currentNode, range.endOffset);
+	while (tailNode === null && currentNode.nextSibling !== null) {
+	    currentNode = currentNode.nextSibling;
+	    searchSentenceTail(currentNode);
+	}
+	if (tailNode === null) {
+	    tailNode = currentNode;
+	    tailIndex = (tailNode.nodeValue) ? tailNode.nodeValue.length : 0;
+	}
+
+	if (headNode === null || tailNode === null) {
+	    alert('headNode or tailNode is null');
+	    return;
+	}
+
+	if (headNode.nodeType === Node.TEXT_NODE) {
+	    var str = headNode.nodeValue.substr(headIndex);
+	    var tmpIndex = str.search(/\S/);
+	    if (tmpIndex > 0) {
+		headIndex += tmpIndex;
+	    }
+	    if (index > 0 && headIndex > 0 &&
+		headNode.nodeValue.substr(headIndex - 1, 1).search(/\s/) >= 0) {
+		headIndex--;
+	    }
+	}
+	if (tailNode.nodeType === Node.TEXT_NODE) {
+	    var str = tailNode.nodeValue.substr(0, tailIndex);
+	    var tmpIndex = str.search(/\s$/);
+	    if (tmpIndex > 0) {
+		tailIndex = tmpIndex;
+	    }
+	}
+
+	range.setStart(headNode, headIndex);
+	range.setEnd(tailNode, tailIndex);
 	return;
     }
 
-
-    var currentNode = range.startContainer;
-    searchSentenceHead(currentNode, range.startOffset);
-
-    while (headNode === null && currentNode.previousSibling !== null) {
-	currentNode = currentNode.previousSibling;
-	searchSentenceHead(currentNode);
+    var range;
+    for (var i = 0; i < content.getSelection().rangeCount; i++) {
+	range = content.getSelection().getRangeAt(i);
+	content.getSelection().removeRange(range);
+	stretchRange(range, i);
+	content.getSelection().addRange(range);
     }
-    if (headNode === null) {
-	headNode = currentNode;
-	headIndex = 0;
-    }
-
-    currentNode = range.endContainer;
-    searchSentenceTail(currentNode, range.endOffset);
-
-    while (tailNode === null && currentNode.nextSibling !== null) {
-	currentNode = currentNode.nextSibling;
-	searchSentenceTail(currentNode);
-    }
-    if (tailNode === null) {
-	tailNode = currentNode;
-	tailIndex = (tailNode.nodeValue) ? tailNode.nodeValue.length : 0;
-    }
-
-    if (headNode === null || tailNode === null) {
-	alert('headNode or tailNode is null');
-	return;
-    }
-
-    if (headNode.nodeType === Node.TEXT_NODE) {
-	var str = headNode.nodeValue.substr(headIndex);
-	var tmpIndex = str.search(/\S/);
-	if (tmpIndex > 0) {
-	    headIndex += tmpIndex;
-	}
-    }
-    if (tailNode.nodeType === Node.TEXT_NODE) {
-	var str = tailNode.nodeValue.substr(0, tailIndex);
-	var tmpIndex = str.search(/\s$/);
-	if (tmpIndex > 0) {
-	    tailIndex = tmpIndex;
-	}
-    }
-
-    content.getSelection().removeAllRanges();
-    range.setStart(headNode, headIndex);
-    range.setEnd(tailNode, tailIndex);
-    content.getSelection().addRange(range);
 
     return;
 }
